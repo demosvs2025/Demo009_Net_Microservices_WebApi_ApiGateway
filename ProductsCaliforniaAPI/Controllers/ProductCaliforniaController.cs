a7866eff8cd78bcbcfad820129da1325c777781e
dev1 <dev1>
2024-09-01 18:07:48 -0600

    Add project files.

---
+++ ProductsCaliforniaAPI/Controllers/ProductCaliforniaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductsCaliforniaAPI.Data;
+using ProductsCaliforniaAPI.Models;
+
+namespace ProductsCaliforniaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCaliforniaController : ControllerBase
+    {
+        private readonly ProductsCaliforniaDbContext context;
+
+        public ProductCaliforniaController(ProductsCaliforniaDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ProductCalifornia>> GetProductsCalifornia()
+        {
+            return Ok(context.ProductsCalifornia.ToList());
+        }
+    }
+}
